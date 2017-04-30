a96f2bce294d0ad3828ee15fa3af065d7768c70e
lug <gdom23@gmail.com>
2017-04-30 20:20:37 -0300

    stubs no longer needed

--- Trabalho1/stubs.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-def parse_sgml_stub():
-  yield {"id": "12345", "text": "florzinha bonita no pátio ao Sol"}
-
-def recupera_consultas_stub():
-  yield "que bonito, hein"
