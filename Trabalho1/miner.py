91166d0688bb1726e1f7a79c26f29b971103db50
lug <gdom23@gmail.com>
2017-04-30 17:19:07 -0300

    Adicionando o programa que trata da entrada e retorna objetos contendo id e texto dos documentos

---
+++ Trabalho1/miner.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from re import match
+
+# Esta função foi feita com MUITA boa fé
+# Os arquivos tem que estar formatados perfeitamente
+def find_all(file):
+  y = {}
+  _text = []
+  reading = False
+  for line in file:
+    
+    if line.startswith("<DOCID>"):
+      y["id"] = match("<DOCID>(?P<_ID>.*)</DOCID>",line).group('_ID')
+
+    elif line.strip() == "<TEXT>":
+      reading = True
+
+    elif line.strip() == "</TEXT>":
+      reading = False
+      y["text"] = "".join(_text)    
+      _text.clear()
+
+    elif reading:
+      _text.append(line)
+
+    if "id" in y and "text":
+      yield y
+      y = {}
+
+
+def parse_arquivos(dir="./colecao_teste"):
+  # assumindo que dir seja um diretorio contendo arquivos de entrada 
+  for file in Path(dir).iterdir():
+    with open(str(file), mode='r', encoding="latin_1") as f:
+      for doc in find_all(f):
+        yield doc
+
+
+if __name__ == '__main__':
+  for doc in parse_arquivos('./colecao_teste'):
+    print(doc)
